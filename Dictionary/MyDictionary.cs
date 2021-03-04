52a03e6a2abcbd5ffec46de7765e98c91eb79c79
tunahan karaçoban <krctunahan05@gmail.com>
2021-03-04 17:41:52 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T,K>
+    {
+        T[] numbers;
+        K[] names;
+        public MyDictionary()
+        {
+            numbers =new T[0];
+            names = new K[0];
+        }
+        public void Add(T number,K name)
+        {
+            T[] tempArray = numbers;
+            numbers = new T[numbers.Length + 1];
+
+            K[] tempArray2 = names;
+            names = new K[names.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                numbers[i] = tempArray[i];
+            }
+            numbers[numbers.Length - 1] = number;
+
+
+
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                names[i] = tempArray2[i];
+            }
+            names[names.Length - 1] = name;
+        }
+        public int count
+        {
+            get { return numbers.Length; }
+
+        }
+    }
+    
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cities = new MyDictionary<int, string>();
+            cities.Add(05, "Amasya");
+            cities.Add(06, "Ankara");
+            cities.Add(34, "İstanbul");
+            Console.WriteLine(cities.count);
+        }
+    }
+}
